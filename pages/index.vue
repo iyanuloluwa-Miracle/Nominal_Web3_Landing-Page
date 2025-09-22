cf30a3823a33f527db0894aae1e520f07ac7e1f9
iyanuloluwa-Miracle <iyanudina@gmail.com>
2025-09-22 11:27:44 +0100

    feat: add SolutionSection component to index page

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,12 @@
   <div>
     <HeroSection />
     <ProblemSection />
+    <SolutionSection />
   </div>
 </template>
 
 <script setup>
 import HeroSection from '~/components/HeroSection.vue'
 import ProblemSection from '~/components/ProblemSection.vue'
+import SolutionSection from '~/components/SolutionSection.vue'
 </script>
